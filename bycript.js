c713a715cc113f7f07c333d06a67f086f399e81c
Soumojyoti Ganguly <soumojyotiganguly270@gmail.com>
2020-01-24 11:06:08 +0530

    Update bycript.js

--- bycript.js
+++ bycript.js
@@ -9,11 +9,8 @@ var crypto = require('crypto');
 
 var promises = require('./promises');
 
-/// generate a salt (sync)
-/// @param {Number} [rounds] number of rounds (default 10)
-/// @return {String} salt
 module.exports.genSaltSync = function genSaltSync(rounds, minor) {
-    // default 10 rounds
+    
     if (!rounds) {
         rounds = 10;
     } else if (typeof rounds !== 'number') {
@@ -30,21 +27,19 @@ module.exports.genSaltSync = function genSaltSync(rounds, minor) {
     return bindings.gen_salt_sync(minor, rounds, crypto.randomBytes(16));
 };
 
-/// generate a salt
-/// @param {Number} [rounds] number of rounds (default 10)
-/// @param {Function} cb callback(err, salt)
+
 module.exports.genSalt = function genSalt(rounds, minor, cb) {
     var error;
 
-    // if callback is first argument, then use defaults for others
+    
     if (typeof arguments[0] === 'function') {
-        // have to set callback first otherwise arguments are overriden
+       
         cb = arguments[0];
         rounds = 10;
         minor = 'b';
-    // callback is second argument
+
     } else if (typeof arguments[1] === 'function') {
-        // have to set callback first otherwise arguments are overriden
+       
         cb = arguments[1];
         minor = 'b';
     }
@@ -53,11 +48,11 @@ module.exports.genSalt = function genSalt(rounds, minor, cb) {
         return promises.promise(genSalt, this, [rounds, minor]);
     }
 
-    // default 10 rounds
+   
     if (!rounds) {
         rounds = 10;
     } else if (typeof rounds !== 'number') {
-        // callback error asynchronously
+       
         error = new Error('rounds must be a number');
         return process.nextTick(function() {
             cb(error);
@@ -83,10 +78,7 @@ module.exports.genSalt = function genSalt(rounds, minor, cb) {
     });
 };
 
-/// hash data using a salt
-/// @param {String} data the data to encrypt
-/// @param {String} salt the salt to use when hashing
-/// @return {String} hash
+
 module.exports.hashSync = function hashSync(data, salt) {
     if (data == null || salt == null) {
         throw new Error('data and salt arguments required');
@@ -103,10 +95,7 @@ module.exports.hashSync = function hashSync(data, salt) {
     return bindings.encrypt_sync(data, salt);
 };
 
-/// hash data using a salt
-/// @param {String} data the data to encrypt
-/// @param {String} salt the salt to use when hashing
-/// @param {Function} cb callback(err, hash)
+
 module.exports.hash = function hash(data, salt, cb) {
     var error;
 
@@ -124,8 +113,7 @@ module.exports.hash = function hash(data, salt, cb) {
         });
     }
 
-    // cb exists but is not a function
-    // return a rejecting promise
+    
     if (cb && typeof cb !== 'function') {
         return promises.reject(new Error('cb must be a function or null to return a Promise'));
     }
@@ -158,10 +146,6 @@ module.exports.hash = function hash(data, salt, cb) {
     return bindings.encrypt(data, salt, cb);
 };
 
-/// compare raw data to hash
-/// @param {String} data the data to hash and compare
-/// @param {String} hash expected hash
-/// @return {bool} true if hashed data matches hash
 module.exports.compareSync = function compareSync(data, hash) {
     if (data == null || hash == null) {
         throw new Error('data and hash arguments required');
@@ -174,10 +158,7 @@ module.exports.compareSync = function compareSync(data, hash) {
     return bindings.compare_sync(data, hash);
 };
 
-/// compare raw data to hash
-/// @param {String} data the data to hash and compare
-/// @param {String} hash expected hash
-/// @param {Function} cb callback(err, matched) - matched is true if hashed data matches hash
+
 module.exports.compare = function compare(data, hash, cb) {
     var error;
 
@@ -195,8 +176,7 @@ module.exports.compare = function compare(data, hash, cb) {
         });
     }
 
-    // cb exists but is not a function
-    // return a rejecting promise
+  
     if (cb && typeof cb !== 'function') {
         return promises.reject(new Error('cb must be a function or null to return a Promise'));
     }
